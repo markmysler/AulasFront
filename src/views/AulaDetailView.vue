6bd142fffcc2fa5d433a14830c6765f4c933bee0
Mark Mysler <myslermark@gmail.com>
2024-01-22 13:25:57 -0300

    reserva:id aula:id y el header ahora se cierra solo

---
+++ src/views/AulaDetailView.vue
@@ -0,0 +1,25 @@
+<template>
+    <p>mostrar datos del aula y calendario del dia para ese aula</p>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    name: 'AulaDetailView',
+    data(){
+        return{
+            aulaData: null,
+            aulaId: '',
+        }
+    },
+    methods:{
+        getAulaData(id, data){
+            // get aula detalle info 
+            // data = lo que llega del server
+        }
+    },
+    mounted(){
+        this.aulaId = this.$route.params.id
+        this.getAulaData(this.aulaId,this.aulaData)
+    }
+}
+</script>
